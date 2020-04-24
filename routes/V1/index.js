3526c2c55a8b521008ee1ec1250358b50d04fc32
Timmy Morrissey <timmy.morrissey@routematch.com>
2020-04-24 21:14:31 +0100

    added token authentication

--- routes/V1/index.js
+++ routes/V1/index.js
@@ -1,15 +1,16 @@
+const tokenValidator = require('@timmymorr/token-validator');
 const express = require('express');
 const controller = require('./controllers/favourites');
 
 const router = express.Router();
 
 router.route('/favourite/:user_id/:id')
-  .get(controller.getFavourite)
-  .patch(controller.updateFavourite)
-  .delete(controller.deleteFavourite)
+  .get(tokenValidator.validateToken, controller.getFavourite)
+  .patch(tokenValidator.validateToken, controller.updateFavourite)
+  .delete(tokenValidator.validateToken, controller.deleteFavourite)
 
 router.route('/favourites/:user_id')
-  .get(controller.getAll)
-  .post(controller.addFavourite)
+  .get(tokenValidator.validateToken, controller.getAll)
+  .post(tokenValidator.validateToken, controller.addFavourite)
 
 module.exports = router;
