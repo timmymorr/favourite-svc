f567072eb101d393f3b155dd69a47a93eff9cbc5
Timmy Morrissey <timmy.morrissey@routematch.com>
2020-05-02 14:09:51 +0100

    added auth header rule

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization, Content-Type');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
